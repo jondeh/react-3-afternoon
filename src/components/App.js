d7773ed01c9bd98c150231822b9dbaff26dab472
jondeh <dehlinjd@gmail.com>
2020-04-29 13:40:24 -0600

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post';
 
 import './App.css';
 
@@ -19,22 +21,23 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`).then(res => this.setState({posts: res.data}))
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then(res => this.setState({posts: res.data}))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(res => this.setState({posts: res.data}))
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {text}).then(res => this.setState({posts: res.data}))
   }
 
   render() {
+    console.log(this.state.posts)
     const { posts } = this.state;
 
     return (
@@ -43,7 +46,10 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
+          {
+            posts.map((e, i) => <Post key={e.id} text={e.text} date={e.date} updatePostFn={this.updatePost} id={e.id} deletePostFn={this.deletePost} />)
+          }
           
         </section>
       </div>
